92277aa4a67261c7635593ed4952b3724dbcd34b
babylee2002 <babylee2002@gmail.com>
2018-09-13 09:54:29 -0400

    blog post app created

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,5 @@
 class PostsController < ApplicationController
+    http_basic_authenticate_with name: "brad", password: "1234", except: [:index, :show]
     def index
         @posts = Post.all
     end
@@ -8,16 +9,38 @@ class PostsController < ApplicationController
     end 
 
     def new
-
+        @post = Post.new
     end
 
     def create
         #render plain: params[:post].inspect
     @post = Post.new(post_params)
-    @post.save
+    if(@post.save)
     redirect_to @post
-
+    else
+        render 'new'
+    end
     end 
+
+    def edit
+        @post = Post.find(params[:id])
+    end
+
+    def update
+        @post = Post.find(params[:id])
+        if(@post.update(post_params))
+            redirect_to @post
+        else 
+            render 'edit'
+        end
+    end 
+
+    def destroy
+        @post = Post.find(params[:id])
+        @post.destroy
+        redirect_to posts_path
+    end 
+
     private def post_params
     params.require(:post).permit(:title, :body)
     end
